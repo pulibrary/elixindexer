94817b1dc81b5ae175b1a0fa7a55c012ec772227
Trey Pendragon <tpendragon@princeton.edu>
2018-04-09 16:42:44 -0700

    Only pull certain Title codes.

--- lib/elixindexer.ex
+++ lib/elixindexer.ex
@@ -28,6 +28,7 @@ defmodule Elixindexer do
     build_record(fields)
   end
 
+  # Skip any fields without record tag.
   def parse_record(_) do
     nil
   end
@@ -37,14 +38,17 @@ defmodule Elixindexer do
     |> Enum.reduce(%{}, &build_record/2)
   end
 
+  # Convert 001 to id
   def build_record({:xmlel, "controlfield", [{"tag", "001"}], [xmlcdata: id]}, acc = %{}) do
     acc
     |> Map.put(:id, id)
   end
 
+  # Convert 245 to title.
   def build_record({:xmlel, "datafield", [{_, _}, {_, _}, {"tag", "245"}], fields}, acc = %{}) do
     title = fields
-    |> Enum.map(&get_data/1)
+    |> Enum.map(&get_data(&1, ["a","b","c","f","g","h","k","n","p","s"]))
+    |> Enum.filter(fn(x) -> x != nil end)
     |> Enum.join(" ")
     acc
     |> Map.put(:title, title)
@@ -54,8 +58,16 @@ defmodule Elixindexer do
     acc
   end
 
-  def get_data({:xmlel, _, _, [xmlcdata: data]}) do
-    data
+  def get_data({:xmlel, _, [{"code", code}], [xmlcdata: data]}, codes) do
+    if Enum.member?(codes, code) do
+      data
+    else
+      nil
+    end
+  end
+
+  def get_data(_, _) do
+    nil
   end
 
   # {:xmlel, "datafield", [{"ind1", "1"}, {"ind2", "0"}, {"tag", "245"}],
