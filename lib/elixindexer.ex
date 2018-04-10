60c39ffdb4e9f926cf4d9f8c3bf3ae55f32eb665
Trey Pendragon <tpendragon@princeton.edu>
2018-04-10 15:16:57 -0700

    Convert to solr.

--- lib/elixindexer.ex
+++ lib/elixindexer.ex
@@ -3,5 +3,32 @@ defmodule Elixindexer do
   def parse_records(file_name) do
     {:ok, handle} = File.open(file_name)
     result = MarcParser.parse_marc(handle)
+             |> Enum.map(&solrize/1)
+  end
+
+  def solrize(%MarcParser.Record{fields: fields}) do
+    id = fields["001"].value
+    title = fields["245"]
+            |> get_subfields("abcfghknps")
+            |> Enum.join(" ")
+    %{
+      id: id,
+      title: title
+    }
+  end
+
+  def get_subfields(%MarcParser.DataField{subfields: subfields}, fields) do
+    fields
+    |> String.graphemes
+    |> Enum.map(&get_subfield_value(subfields[&1]))
+    |> Enum.filter(fn (x) -> x != nil end)
+  end
+
+  def get_subfield_value(%MarcParser.SubField{value: value}) do
+    value
+  end
+
+  def get_subfield_value(_) do
+    nil
   end
 end
--- test/elixindexer_test.exs
+++ test/elixindexer_test.exs
@@ -9,8 +9,8 @@ defmodule ElixindexerTest do
   test "parse_records" do
     records = Elixindexer.parse_records("small_set.mrc")
     assert length(records) == 31
-    # record = hd(records)
-    # assert(%{id: "345682"} = record)
-    # assert(%{title: "Opportunity in crisis : money and power in world politics 1986-88 / Michael M. White."} = record)
+    record = hd(records)
+    assert(%{id: "6000001"} = record)
+    assert(%{title: "Bach und die deutsche Tradition des Komponierens : Wirklichkeit und Ideologie ; Festschrift Martin Geck zum 70. Geburtstag ; Bericht über das 6. Dortmunder Bach-Symposion 2006 / herausgegeben von Reinmar Emans und Wolfram Steinbeck."} = record)
   end
 end
