1ffe1b46bcd38c01f50a323ed78ed16c9b556801
Trey Pendragon <tpendragon@princeton.edu>
2018-04-11 16:20:15 -0700

    Improve parse time.

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Elixindexer.MixProject do
   defp deps do
     [
       {:benchfella, "~> 0.3.0", only: [:dev, :test]},
-      {:flow, "~> 0.13"}
+      {:flow, "~> 0.13"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
