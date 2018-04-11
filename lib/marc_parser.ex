1a58385deb52f7e57b146ee6bea55e0b65f0bbdb
Trey Pendragon <tpendragon@princeton.edu>
2018-04-11 16:20:15 -0700

    Improve parse time.

--- lib/marc_parser.ex
+++ lib/marc_parser.ex
@@ -13,7 +13,7 @@ defmodule MarcParser do
   end
   @leader_length 24
   @directory_entry_length 12
-  @subfield_indicator 0x1F
+  @subfield_indicator <<0x1F>>
   def parse_marc(marc_handle) do
     MarcParser.Stream.from_handle(marc_handle)
     |> Flow.from_enumerable
@@ -21,24 +21,27 @@ defmodule MarcParser do
   end
 
   def parse_record(marc_record) do
-    leader = marc_record |> Kernel.binary_part(0, @leader_length)
-    base_address = leader |> Kernel.binary_part(12, 5) |> String.to_integer
-    directory = marc_record |> Kernel.binary_part(@leader_length, base_address-@leader_length-1)
+    <<leader::binary-size(@leader_length), _::binary>> = marc_record
+    <<_::binary-size(12), base_address::binary-size(5), _::binary>> = leader
+    base_address = base_address |> :erlang.binary_to_integer
+    width = base_address-@leader_length-1
+    <<_::binary-size(@leader_length), directory::binary-size(width), _::binary>> = marc_record
     num_fields = div(byte_size(directory), @directory_entry_length)
-    fields = Enum.reduce(0..(num_fields-1), %{}, &extract_field(marc_record, directory, base_address, &1, &2))
+    subfield_pattern = :binary.compile_pattern(<<0x1F>>)
+    fields = Enum.reduce(0..(num_fields-1), %{}, &extract_field(marc_record, directory, base_address, subfield_pattern, &1, &2))
     %MarcParser.Record{leader: leader, fields: fields}
   end
 
-  def extract_field(marc_record, directory, base_address, field_num, acc) do
+  def extract_field(marc_record, directory, base_address, subfield_pattern, field_num, acc) do
     entry_start = field_num * @directory_entry_length
-    entry_end = entry_start + @directory_entry_length
-    entry = Kernel.binary_part(directory, entry_start, entry_end-entry_start)
-    tag = Kernel.binary_part(entry, 0, 3)
-    field_length = Kernel.binary_part(entry, 3, 4) |> String.to_integer
-    field_offset = Kernel.binary_part(entry, 7, 5) |> String.to_integer
+    <<_::binary-size(entry_start), tag::binary-size(3), field_length::binary-size(4), field_offset::binary-size(5), _::binary>> = directory
+    # entry = Kernel.binary_part(directory, entry_start, entry_width)
+    field_length = field_length |> :erlang.binary_to_integer
+    field_length = field_length - 1
+    field_offset = field_offset |> :erlang.binary_to_integer
     field_start = base_address + field_offset
-    field_data = Kernel.binary_part(marc_record, field_start, field_length) |> remove_field_end
-    field = generate_field(tag, field_data)
+    <<_::binary-size(field_start), field_data::binary-size(field_length), _::binary>> = marc_record
+    field = generate_field(tag, field_data, subfield_pattern)
     case Map.fetch(acc, tag) do
       :error        -> acc |> Map.put(tag, [field])
       {:ok, value}  -> acc |> Map.put(tag, value ++ [field])
@@ -46,44 +49,44 @@ defmodule MarcParser do
     end
   end
 
-  def generate_field(tag = "001", field_data) do
+  def generate_field(tag = "001", field_data, _) do
     generate_control_field(tag, field_data)
   end
-  def generate_field(tag = "002", field_data) do
+  def generate_field(tag = "002", field_data, _) do
     generate_control_field(tag, field_data)
   end
-  def generate_field(tag = "003", field_data) do
+  def generate_field(tag = "003", field_data, _) do
     generate_control_field(tag, field_data)
   end
-  def generate_field(tag = "004", field_data) do
+  def generate_field(tag = "004", field_data, _) do
     generate_control_field(tag, field_data)
   end
-  def generate_field(tag = "005", field_data) do
+  def generate_field(tag = "005", field_data, _) do
     generate_control_field(tag, field_data)
   end
-  def generate_field(tag = "006", field_data) do
+  def generate_field(tag = "006", field_data, _) do
     generate_control_field(tag, field_data)
   end
-  def generate_field(tag = "007", field_data) do
+  def generate_field(tag = "007", field_data, _) do
     generate_control_field(tag, field_data)
   end
-  def generate_field(tag = "008", field_data) do
+  def generate_field(tag = "008", field_data, _) do
     generate_control_field(tag, field_data)
   end
-  def generate_field(tag = "009", field_data) do
+  def generate_field(tag = "009", field_data, _) do
     generate_control_field(tag, field_data)
   end
-  def generate_field(tag, field_data) do
-    generate_data_field(tag, field_data)
+  def generate_field(tag, field_data, subfield_pattern) do
+    generate_data_field(tag, field_data, subfield_pattern)
   end
 
   def generate_control_field(tag, field_data) do
     %MarcParser.ControlField{tag: tag, value: field_data}
   end
 
-  def generate_data_field(tag, field_data) do
-    [indicators | subfields] = field_data |> :binary.split([<<@subfield_indicator>>], [:global])
-    [indicator1, indicator2] = indicators |> String.codepoints
+  def generate_data_field(tag, field_data, subfield_pattern) do
+    [indicators | subfields] = field_data |> :binary.split(subfield_pattern, [:global])
+    <<indicator1::binary-size(1), indicator2::binary-size(1)>> = indicators
     subfields = subfields
                 |> Enum.reduce([], &generate_subfield/2)
     %MarcParser.DataField{tag: tag, indicator1: indicator1, indicator2: indicator2, subfields: subfields}
@@ -93,24 +96,10 @@ defmodule MarcParser do
     if byte_size(subfield_data) == 0 do
       acc
     else
-      code = binary_part(subfield_data, 0, 1)
-      value = binary_part(subfield_data, 1, byte_size(subfield_data)-1)
+      <<code::binary-size(1), value::binary>> = subfield_data
       field = %MarcParser.SubField{code: code, value: value}
       acc ++ [field]
-      #case Map.fetch(acc, code) do
-      #  :error        -> acc |> Map.put(code, [field])
-      #  {:ok, value}  -> acc |> Map.put(code, value ++ [field])
-      #  _             -> acc
-      #end
     end
   end
-
-  def remove_field_end(field_data) do
-    case :binary.last(field_data) do
-      0x1E -> binary_part(field_data, 0, byte_size(field_data) - 1)
-      _ -> field_data
-    end
-  end
-
 
 end
