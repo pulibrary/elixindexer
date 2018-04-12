62843376031178cf8eadfcef83a456b0095ba0e1
christinach <actspatial@gmail.com>
2018-04-12 17:09:51 -0400

    Adds subject and author fields

--- test/elixindexer_test.exs
+++ test/elixindexer_test.exs
@@ -8,8 +8,14 @@ defmodule ElixindexerTest do
     record = records |> Enum.find(fn(x) -> x.id == "6000001" end)
     assert record.id == "6000001"
     assert record.title_display == "Bach und die deutsche Tradition des Komponierens : Wirklichkeit und Ideologie ; Festschrift Martin Geck zum 70. Geburtstag ; Bericht über das 6. Dortmunder Bach-Symposion 2006 / herausgegeben von Reinmar Emans und Wolfram Steinbeck."
-    assert record.author_display == "Dortmunder Bach-Symposion (6th : 2006)"
+    assert record.author_display == ["Dortmunder Bach-Symposion (6th : 2006)"]
+    assert record.author_s == ["Dortmunder Bach-Symposion (6th : 2006)",
+                                "Emans, Reinmar",
+                                "Steinbeck, Wolfram"]
+    assert record.subject_display == [
+                                "Bach, Johann Sebastian, 1685-1750—Congresses",
+                                "Bach, Johann Sebastian, 1685-1750—Influence—Congresses"
+                              ]
   end
 end
 
-# 100aqbcdk:110abcdfgkln:111abcdfgklnpq
