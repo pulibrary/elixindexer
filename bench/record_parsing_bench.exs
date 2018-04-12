d81a66a998f1f0ac2d10301192131daa854a33b1
Trey Pendragon <tpendragon@princeton.edu>
2018-04-12 09:44:48 -0700

    Revert benchmark.

--- bench/record_parsing_bench.exs
+++ bench/record_parsing_bench.exs
@@ -1,12 +1,12 @@
  defmodule RecordParsingBench do
    use Benchfella
 
-   # bench "parse large file" do
-   #   Elixindexer.parse_records(["20.mrc"])
-   # end
-   bench "parse everything" do
-     File.ls!("full_dump")
-     |> Enum.map(fn (x) -> "full_dump/#{x}" end)
-     |> Elixindexer.parse_records
+   bench "parse large file" do
+     Elixindexer.parse_records(["20.mrc"])
    end
+   # bench "parse everything" do
+   #   File.ls!("full_dump")
+   #   |> Enum.map(fn (x) -> "full_dump/#{x}" end)
+   #   |> Elixindexer.parse_records
+   # end
  end
